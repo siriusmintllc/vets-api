fd9d256b7cf0486257cafa1775b596f052f6b4bc
Wyatt Walter <wyattwalter@gmail.com>
2017-09-28 20:57:31 -0500

    make linter happy and exclude test dir from coverage report

--- config/environments/benchmark.rb
+++ config/environments/benchmark.rb
@@ -1,1 +1,2 @@
+# frozen_string_literal: true
 require File.expand_path('../production', __FILE__)
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -26,6 +26,7 @@ unless ENV['NOCOVERAGE']
     add_filter 'lib/config_helper.rb'
     add_filter 'lib/feature_flipper.rb'
     add_filter 'spec'
+    add_filter 'test'
     add_filter 'vendor'
     SimpleCov.minimum_coverage_by_file 90
     SimpleCov.refuse_coverage_drop
